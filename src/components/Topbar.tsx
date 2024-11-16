672ec3a25a8e7b1e9b9bfc4498bb2e054ac3e65a
Mau Ulisse Luz <maulisseluz@gmail.com>
2024-11-16 04:34:13 -0300

    feat: added first version of hero section

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,14 +1,13 @@
 import TopbarLinks from './topbar/Links'
 import Logo_light from '../assets/imgs/Symetria.png'
-import { animatedGradient } from '../utils/styles'
 
 function Topbar() {
     return (
-        <div className="w-full  z-20 fixed flex justify-center bg-transparent">
+        <div className="w-full  z-20 fixed flex justify-center bg-transparent py-3">
             <div className="w-[1440px] flex justify-end">
                 <div className="relative w-[40rem] h-[80px] p-4">
-                    <div className={`w-full h-full ${animatedGradient} blur-md opacity-65`}></div>
-                    <div className="w-[37.5rem] h-[50px] absolute top-4 left-5 flex items-center px-2 rounded-[10px] bg-sym_gray-50">
+                    {/* <div className={`w-full h-full ${animatedGradient} blur opacity-90`}></div> */}
+                    <div className="w-[38rem] h-[50px] absolute top-4 left-4 flex items-center px-2 rounded-[10px] bg-sym_gray-50">
                         <img src={Logo_light} alt="logo" className='w-10 h-10 animate-pulse' />
                         <TopbarLinks />
                         <img src={Logo_light} alt="logo" className='w-10 h-10 opacity-0' />
